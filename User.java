7a90e7a32c008992f22a413cfcb103baca2d64de
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-03-02 11:32:14 +0100

    solution

---
+++ User.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+import java.util.ArrayList;
+
+
+
+public class User {
+    private Date birthDate;
+    private int size;
+    private ArrayList<Photo> photos;
+    public static void main(String[] args) {}
+} 
